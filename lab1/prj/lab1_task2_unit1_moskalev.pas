600ac1cf817bd9dc53e105aee225d5304d175162
Anton Moskalev <60475915+mag1lan199@users.noreply.github.com>
2020-03-22 13:10:40 +0200

    Переписан код.
    З коду прибран валідатор.
    Включено окремим модулем валідатор.
    Виконання перевірки валідності числа за допомогою валідатора.
    Фікс багу з порожніми рядками.

--- lab1/prj/lab1_task2_unit1_moskalev.pas
+++ lab1/prj/lab1_task2_unit1_moskalev.pas
@@ -6,16 +6,17 @@ interface
 
 uses
   Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, ExtCtrls,
-  Menus, lab1_task2_unit2_moskalev;
+  Menus, lab1_task2_unit2_moskalev, lab1_task2_validator_moskalev, math;
 
 type
 
   { TForm1 }
 
   TForm1 = class(TForm)
-    Label1: TLabel;
-    Label6: TLabel;
-    Label7: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+    Label4: TLabel;
+    Label5: TLabel;
     MainMenu1: TMainMenu;
     About: TMenuItem;
     ResButton: TButton;
@@ -44,60 +45,31 @@ implementation
 
 procedure TForm1.ResButtonClick(Sender: TObject);
 const Pi = 3.14159;
-const Numbers = ['0'..'9', ',', '.', '-'];
 var
-  tempSTR : string[255];
-  i : byte;
-  x_coms, y_coms, z_coms : boolean;
-
+  tempSTR : string;
+  x,y,z : extended;
 begin
-  x_coms := false;
-  y_coms := false;
-  z_coms := false;
-  tempSTR := x_input.text;
-  for i:=1 to length(tempSTR) do
-  //Проверка числа Х
-  begin
-  if not(tempSTR[i] in Numbers) then
-  begin ResLabel.Caption := 'Помилка. Введене некоректне число (Х).'; exit; end;
-  if ((tempSTR[i] = ',') or (tempSTR[i] = '.')) and (x_coms = true) then
-  begin ResLabel.Caption := 'Помилка. Введене некоректне число (Х).'; exit; end;
-  if (tempSTR[i] = '.') or (tempSTR[i] = ',') then
-  begin tempSTR[i] := ','; x_input.text := tempSTR; x_coms := true; end;
-  end;
-  //Проверка числа У
-  tempSTR := y_input.text;
-  for i:=1 to length(tempSTR) do
-  begin
-  if not(tempSTR[i] in Numbers) then
-  begin ResLabel.Caption := 'Помилка. Введене некоректне число (Y).'; exit; end;
-  if ((tempSTR[i] = ',') or (tempSTR[i] = '.')) and (y_coms = true) then
-  begin ResLabel.Caption := 'Помилка. Введене некоректне число (Y).'; exit; end;
-  if (tempSTR[i] = '.') or (tempSTR[i] = ',') then
-  begin tempSTR[i] := ','; y_input.text := tempSTR; y_coms := true; end;
-  end;
-  //Проверка числа З
-  tempSTR := z_input.text;
-  for i:=1 to length(tempSTR) do
-  begin
-  if not(tempSTR[i] in Numbers) then
-  begin ResLabel.Caption := 'Помилка. Введене некоректне число (Z).'; exit; end;
-  if ((tempSTR[i] = ',') or (tempSTR[i] = '.')) and (z_coms = true) then
-  begin ResLabel.Caption := 'Помилка. Введене некоректне число (Z).'; exit; end;
-  if (tempSTR[i] = '.') or (tempSTR[i] = ',') then
-  begin tempSTR[i] := ','; z_input.text := tempSTR; z_coms := true; end;
-  end;
+  if (x_input.text = '') or (y_input.text = '') or (z_input.text = '') then
+  begin reslabel.caption := 'Задайте усі значення.'; exit; end;
+  tempSTR := x_input.text;                                                  // X
+  if ReturnNum(tempSTR) = 'N' then
+  begin reslabel.caption := 'Невірне число Х.'; exit; end;
+  x := StrToFloat(ReturnNum(tempSTR));
+  tempSTR := y_input.text;                                                  // Y
+  if ReturnNum(tempSTR) = 'N' then
+  begin reslabel.caption := 'Невірне число Y.'; exit; end;
+  y := StrToFloat(ReturnNum(tempSTR));
+  tempSTR := z_input.text;                                                  // Z
+  if ReturnNum(tempSTR) = 'N' then
+  begin reslabel.caption := 'Невірне число Z.'; exit; end;
+  z := StrToFloat(ReturnNum(tempSTR));
 
-  //Ограничение чисел
-  if (StrToFloat(x_input.text) > 142) or (StrToFloat(x_input.text) < 1) then
-  begin ResLabel.Caption := 'Число не може бути меншим за 1 та більшим за 142. (Х)'; exit; end;
-  if (StrToFloat(y_input.text) > 142) or (StrToFloat(x_input.text) < -142) then
-  begin ResLabel.Caption := 'Число не може бути меншим за -142 та більшим за 142. (Y)'; exit; end;
-  if (StrToFloat(z_input.text) > 142) or (StrToFloat(x_input.text) < 1) then
-  begin ResLabel.Caption := 'Число не може бути меншим за 1 та більшим за 142. (Z)'; exit; end;
+
+  if Interv(x,y,z) = false then
+  begin reslabel.caption := 'Одне з чисел не відповідає діапазону.'; exit; end;
 
   //Формула
-  ResLabel.Caption := 'Результат: ' + FloatToStr((cos(StrToFloat(x_input.text)+StrToFloat(y_input.text)) - sqrt(exp(abs(StrToFloat(x_input.text))*ln(StrToFloat(z_input.text)))) + sqrt(pi*( exp(StrToFloat(y_input.text)*ln(StrToFloat(x_input.text)))))));
+  ResLabel.Caption := 'Результат: ' + FloatToStr((cos(x+y)) - (sqrt(power(z,abs(x)))) + (sqrt(Pi*power(x,y))));
 
 end;
 
